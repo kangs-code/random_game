4aab04ccc80a756f6f96a6c7a42ef5ccca3960c1
kimen <kmkang0619@gmail.com>
2025-01-07 10:36:03 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const scope = document.querySelector(".scope");
 const choose = document.querySelector(".choose");
 const Text = document.querySelector(".Text");
 const result = document.querySelector(".result");
+const HIDDEN = "hidden"
 
 
 
@@ -15,16 +16,16 @@ function submitEvent(event){
     const scope_number = parseInt(scope.value);
     let machine_number = Math.ceil(Math.random()*scope_number);
     Text.innerText = `You chose: ${choose.value}, the machine chose: ${machine_number}`;
-    Text.classList.remove("hidden");
+    Text.classList.remove(HIDDEN);
 
 
     if(parseInt(choose.value) === machine_number){
         result.innerHTML = "<div style='margin-bottom:0px; margin-top:-20px'>You won!</div>"
-        result.classList.remove("hidden");
+        result.classList.remove(HIDDEN);
     }
     else{
         result.innerHTML = "<div style='margin-bottom:0px; margin-top:-20px'>You lost!</div>"
-        result.classList.remove("hidden")
+        result.classList.remove(HIDDEN)
     }
 }
 
